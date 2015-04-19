d8e56681347584ed2c45245c010f9ba1bbc0516e
robnagler <github@q33.us>
2015-04-19 15:58:11 -0600

    pyenv not setup right; fix build of qt

--- darwin/build-user-vagrant.sh
+++ darwin/build-user-vagrant.sh
@@ -4,51 +4,56 @@
 # Install the user which runs RadTrack
 #
 set -e
-qmake=/usr/lib64/qt4/bin/qmake
 
 . /cfg/build-env.sh
 
 build_home_env
 
+# Adds bivio_* commands
 . ~/.bashrc
+
 # This line stops a warning from the pyenv installer
 bivio_path_insert ~/.pyenv/bin 1
 bivio_pyenv_2
 
-# get radtrack
+# Adds pyenv functions now that pyenv exists
+. ~/.bashrc
+
+# RadTrack install
 mkdir -p ~/src/radiasoft
 cd ~/src/radiasoft
 git clone -q ${BIVIO_GIT_SERVER-https://github.com}/radiasoft/radtrack.git
 cd radtrack
+radtrack_dir=$(pwd)
 
 # local pyenv
 bivio_pyenv_local
 pyenv activate radtrack
-radtrack=$(pwd)
 
 # Install sip and Qt4
-url_base=https://depot.radiasoft.org/foss
-tmp=/var/tmp/$USER$$
-mkdir $tmp
-cd $tmp
-base=sip-4.16.6
-curl -s -L $url_base/$base.tar.gz | tar xzf -
-cd $base
-python configure.py --incdir="$VIRTUAL_ENV/include"
-make
-make install
-cd ..
-rm -rf $base
-base=PyQt-x11-gpl-4.11.3
-curl -s -L $url_base/$base.tar.gz | tar xzf -
-cd $base
-python configure.py --confirm-license -q "$qmake"
-make
-make install
-cd
-rm -rf $tmp
+build_qt_pkg() {
+    local tgz=$1.tar.gz
+    shift
+    # Put tmp local to user, since for dev, we will just userdel -r vagrant
+    # in compile-debug loop.
+    local tmp=~/build_qt_pkg
+    mkdir "$tmp"
+    cd "$tmp"
+    curl -s -L -O "https://depot.radiasoft.org/foss/$tgz"
+    tar xzf "$tgz"
+    rm -f "$tgz"
+    cd *
+    python configure.py "$@"
+    make
+    make install
+    cd -
+    rm -rf "$tmp"
+}
 
-# RadTrack install
-cd $radtrack
+build_qt_pkg sip --incdir="$VIRTUAL_ENV/include"
+build_qt_pkg PyQt4 --confirm-license -q /usr/lib64/qt4/bin/qmake
+
+# SDDS install
+cd $radtrack_dir
 rm -f radtrack/dcp/sdds*
-cp install/fedora/sdds* $(python -c 'from distutils.sysconfig import get_python_lib as x; print x()')
+cp /cfg/sdds* $(python -c 'from distutils.sysconfig import get_python_lib as x; print x()')
